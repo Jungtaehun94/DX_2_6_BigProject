b3dd54e2b6d78fdf32eb9eede8588216d027cd58
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-16 14:02:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def mapping_demo():
             % e.reason
         )
 st.subheader(" 실시간 서울시 내 소방서")
-cols =st.columns((1,1,1,12,3))
+cols =st.columns((1,1,1,12))
 # cols[1].metric("","")
 # cols[1].metric("12/29", "-4 ℃", "-1Ｆ")
 # cols[1].metric("12/30", "0 ℃", "3Ｆ")
@@ -121,8 +121,7 @@ cols[2].metric("중랑소방서","184","20")
 cols[2].metric("강북소방서","268","50")
 cols[2].metric("성북소방서","177")
 
-with cols[4]:
-    st.altair_chart(bar_chart, use_container_width=True)
+st.altair_chart(bar_chart, use_container_width=True)
 
 
 
