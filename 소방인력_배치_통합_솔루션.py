7cd292714e3be8ca9843352bf9eb7d39ec9ba266
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-16 20:53:35 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -91,7 +91,7 @@ with cols_subhead[3]:
     st.markdown("""
     <div data-testid="stMetricDelta" class="css-wnm74r e16fv1kl0" style="color: rgb(255, 43, 43);"><svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="e1fb0mya1 css-jhkj9c ex0cdmw0"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path></svg><div class="css-50ug3q e16fv1kl3"> -감소 </div></div>
     """, unsafe_allow_html=True)
-cols =st.columns((12,1,1,1))
+cols =st.columns((11,1,1,1,1))
 # cols[1].metric("","")
 # cols[1].metric("12/29", "-4 ℃", "-1Ｆ")
 # cols[1].metric("12/30", "0 ℃", "3Ｆ")
