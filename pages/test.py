7ad06dbb387e1f83cb0788f025c51816cd28f6d6
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 12:52:46 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.markdown("""$('#target-div').load('http://www.example.com/portfolio.php #portfolio-sports');""", unsafe_allow_html = True)
+st.markdown("""<script>$('#target-div').load('http://www.example.com/portfolio.php #portfolio-sports');</script>""", unsafe_allow_html = True)
