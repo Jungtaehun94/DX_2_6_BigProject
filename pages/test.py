56031cce56292be12dec65860804efb7cfffc7de
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 13:28:34 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,9 +2,9 @@ import streamlit as st
 st.markdown("""
 <body>
 	<div>
-		<iframe scrolling="yes" src=http://www.google.com>
+		<iframe scrolling="yes" src=https://nfds.go.kr/dashboard/monitor.do>
 		</iframe>
 	</div>
 </body>
 """, unsafe_allow_html = True)
-# http://nfds.go.kr/dashboard/monitor.do
+# 
