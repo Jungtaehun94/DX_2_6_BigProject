9f7b11430961e0c4aeaad2dcc591ebb8d7e8330b
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 17:15:07 +0900

    Update 1_🔥실시간_전국_화재발생_현황.py

--- pages/1_🔥실시간_전국_화재발생_현황.py
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -16,7 +16,7 @@ with st.spinner("Loading..."):
     st.markdown("""
     <body>
         <div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
+            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 780px; margin-top: -550px; width: 1200px;">
             </iframe>
         </div>
     </body>
