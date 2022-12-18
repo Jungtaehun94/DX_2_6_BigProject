01732956a431bc901684a7b8526151b3eaccdd3f
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 17:03:33 +0900

    Update 1_🔥실시간_전국_화재발생_현황.py

--- pages/1_🔥실시간_전국_화재발생_현황.py
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -14,7 +14,7 @@ add_logo()
 st.markdown("""
 <div class="holds-the-iframe"><iframe id = fire scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;"></iframe></div>
 .holds-the-iframe {
-  background:url(https://nfds.go.kr/favicon.ico) center center no-repeat;
+  background:url(https://github.com/Jungtaehun94/streramlit_temp_app/raw/main/sample.gif) center center no-repeat;
 }
 """, unsafe_allow_html = True)
 
