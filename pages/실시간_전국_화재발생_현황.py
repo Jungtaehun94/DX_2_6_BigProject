e0817ccddb81583bfdc1f0b4053b4c9590b23aa7
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 15:01:57 +0900

    Update 실시간_전국_화재발생_현황.py

--- pages/실시간_전국_화재발생_현황.py
+++ pages/실시간_전국_화재발생_현황.py
@@ -9,14 +9,15 @@ st.set_page_config(page_icon=Image.open(BytesIO(request.urlopen("https://nfds.go
 add_logo()
 
 with st.spinner("Loading..."):
-        time.sleep(2)
-	
-st.markdown("""
-<body>
+        st.markdown("""
+	<body>
 	<div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-		<iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
-		</iframe>
+	<iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
+	</iframe>
 	</div>
-</body>
-""", unsafe_allow_html = True)
+	</body>
+	""",
+		    unsafe_allow_html = True)
+	
+
 # http://nfds.go.kr/dashboard/monitor.do
