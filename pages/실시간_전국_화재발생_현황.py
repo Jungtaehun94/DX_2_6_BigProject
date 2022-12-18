6dc99e175cac85263264566ab9b7bed28d4f1171
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 16:33:14 +0900

    Update 실시간_전국_화재발생_현황.py

--- pages/실시간_전국_화재발생_현황.py
+++ pages/실시간_전국_화재발생_현황.py
@@ -3,21 +3,22 @@ import streamlit as st
 from io import BytesIO
 from urllib import request
 import time
-from add_logo import add_logo
 
-st.set_page_config(page_icon=Image.open(BytesIO(request.urlopen("https://nfds.go.kr/favicon.ico").read())), layout="wide")
-add_logo()
+url = "https://nfds.go.kr/favicon.ico"
+res = request.urlopen(url).read()
+
+im = Image.open(BytesIO(res))
+st.set_page_config(page_icon=im, layout="wide")
+
 
 with st.spinner("Loading..."):
-        st.markdown("""
-	<body>
-	<div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-	<iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
-	</iframe>
-	</div>
-	</body>
-	""",
-		    unsafe_allow_html = True)
-	
-
+    st.markdown("""
+    <body>
+        <div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
+            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
+            </iframe>
+        </div>
+    </body>
+    """, unsafe_allow_html = True)
+    time.sleep(2)
 # http://nfds.go.kr/dashboard/monitor.do
