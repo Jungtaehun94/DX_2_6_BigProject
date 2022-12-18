035de3cb9c0943275ced03a0969efa794f112203
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 13:20:02 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,8 +1,10 @@
 import streamlit as st
 st.markdown("""
 <body>
+	<div>
 		<iframe scrolling="yes" src=http://nfds.go.kr/dashboard/monitor.do>
 		</iframe>
+	</div>
 </body>
 """, unsafe_allow_html = True)
 # http://nfds.go.kr/dashboard/monitor.do
