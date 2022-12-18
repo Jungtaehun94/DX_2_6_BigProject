453866956211e6e9fc61d04261fa85aee77b5b89
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 13:18:46 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,8 +2,9 @@ import streamlit as st
 st.markdown("""
 <body>
 	<div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-		<iframe scrolling="no" src=http://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 926px;">
+		<iframe scrolling="yes" src=http://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 926px;">
 		</iframe>
 	</div>
 </body>
 """, unsafe_allow_html = True)
+# http://nfds.go.kr/dashboard/monitor.do
