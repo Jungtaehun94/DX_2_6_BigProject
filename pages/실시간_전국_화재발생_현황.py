20ed71020530decb9141d88af66f97dd8b0328bd
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 15:00:10 +0900

    Update 실시간_전국_화재발생_현황.py

--- pages/실시간_전국_화재발생_현황.py
+++ pages/실시간_전국_화재발생_현황.py
@@ -5,30 +5,6 @@ from urllib import request
 import time
 from add_logo import add_logo
 
-url = "https://nfds.go.kr/favicon.ico"
-res = request.urlopen(url).read()
-
-im = Image.open(BytesIO(res))
-st.set_page_config(page_title="실시간 전국 화재발생 ", page_icon=im, layout="wide")
-add_logo()
-st.markdown("""
-<body>
-	<div style="border: 3px solid rgb(255, 255, 255); overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-		<iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; margin-top: -550px; width: 1200px;">
-		</iframe>
-	</div>
-</body>
-""", unsafe_allow_html = True)
-# http://nfds.go.kr/dashboard/monitor.do
-
-
-from PIL import Image
-import streamlit as st
-from io import BytesIO
-from urllib import request
-import time
-from add_logo import add_logo
-
 st.set_page_config(page_icon=Image.open(BytesIO(request.urlopen("https://nfds.go.kr/favicon.ico").read())), layout="wide")
 add_logo()
 
