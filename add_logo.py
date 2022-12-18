9edb0f6eb70bd214ef49a389e8b7abc235e7d10d
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 16:07:34 +0900

    Update add_logo.py

--- add_logo.py
+++ add_logo.py
@@ -6,10 +6,10 @@ def add_logo():
             [data-testid="stSidebarNav"]::before {
                 content: "";
                 background-image: url(https://w.namu.la/s/4c3eef0028388ac1c1de07d5dad05ee01c968eeb8325a1fcdaabafb811dcc073d1528f0d880b9930a37fd23f5a77527d3edf5a529bea27b2214366b3f978e951f474b7e3a82e6f5fe397c9ecedb11ba8);
-                background-size:139px, 36px;
+                background-size:139px, 76px;
                 background-repeat: no-repeat;
                 width:139px;
-                height:36px;
+                height:76px;
                 display:inline-block;
                 margin-left: 20px;
                 font-size: 30px;
