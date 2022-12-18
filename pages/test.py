17f6bcdfa0d904df2268da1808ebae2863afd87c
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-18 12:54:25 +0900

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,2 +1,21 @@
 import streamlit as st
-st.markdown("""<script>$('#target-div').load('http://www.example.com/portfolio.php #portfolio-sports');</script>""", unsafe_allow_html = True)
+st.markdown("""
+<head>
+<style>
+.myDiv {
+  border: 5px outset red;
+  background-color: lightblue;
+  text-align: center;
+}
+</style>
+</head>
+<script>$('#target-div').load('http://www.example.com/portfolio.php #portfolio-sports');</script>
+<body>
+
+<div class="myDiv">
+  <h2>This is a heading in a div element</h2>
+  <p>This is some text in a div element.</p>
+</div>
+
+</body>
+""", unsafe_allow_html = True)
