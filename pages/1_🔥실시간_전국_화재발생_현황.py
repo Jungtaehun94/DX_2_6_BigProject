5730a05a270c8b0c3518b75728f80d449b00614d
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-19 15:21:49 +0900

    Update 1_🔥실시간_전국_화재발생_현황.py

--- pages/1_🔥실시간_전국_화재발생_현황.py
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -12,6 +12,20 @@ im = Image.open(BytesIO(res))
 st.set_page_config(page_icon=im, layout="wide")
 add_logo()
 
+toggle_value = st.sidebar.checkbox("Show additional information")
+if toggle_value:
+    st.markdown("### 한 눈에 보는 화재 현황")
+    st.markdown("""
+    <body>
+        <div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; ">
+            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -600px; width: 1200px;">
+            </iframe>
+        </div>
+    </body>
+    """, unsafe_allow_html = True)
+else:
+    st.markdown("")
+
 with st.spinner("Loading..."):
     st.markdown("""
     <body>
