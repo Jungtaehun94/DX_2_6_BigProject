29c1473224b89189126f2517b817fd1d2ff51434
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-19 17:46:22 +0900

    Add files via upload

---
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import streamlit as st
+from io import BytesIO
+from urllib import request
+import time
+from add_logo import add_logo
+
+url = "https://nfds.go.kr/favicon.ico"
+res = request.urlopen(url).read()
+
+im = Image.open(BytesIO(res))
+st.set_page_config(page_icon=im, layout="wide")
+add_logo()
+
+# Create a title for the collapsible section
+title = "오늘자 화재 현황"
+
+# Create the content that you want to hide/reveal
+# hidden_content = 
+
+# Use the collapsible function to create the fold/unfold button
+expander = st.expander(title)
+
+# Use an if statement to check the state of the expander
+with expander:
+    st.markdown("""<div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; padding:200px;">
+        <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -1200px; width: 1200px;">
+        </iframe>
+    </div>""", unsafe_allow_html = True)
+    
+with st.spinner("Loading..."):
+    st.markdown("""
+    <body>
+        <div style="border: 0px none; overflow: hidden; margin: 15px auto; max-width: 1600px;">
+            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; width: 100%; margin-top: -550px; width: 1200px;">
+            </iframe>
+        </div>
+    </body>
+    """, unsafe_allow_html = True)
+    time.sleep(3)
+    
+# http://nfds.go.kr/dashboard/monitor.do
+# <div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px;"></div>
