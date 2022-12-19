9d70831cae0ffe89ff2adc10ad075c81d2bac9fe
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-19 15:28:28 +0900

    Update 1_🔥실시간_전국_화재발생_현황.py

--- pages/1_🔥실시간_전국_화재발생_현황.py
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -11,19 +11,16 @@ res = request.urlopen(url).read()
 im = Image.open(BytesIO(res))
 st.set_page_config(page_icon=im, layout="wide")
 add_logo()
-toggle_value = st.sidebar.checkbox("Show additional information")
-if toggle_value:
-    st.markdown("### 한 눈에 보는 화재 현황")
-    st.markdown("""
-    <body>
-        <div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; ">
-            <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -600px; width: 1200px;">
-            </iframe>
-        </div>
-    </body>
-    """, unsafe_allow_html = True)
-else:
-    st.markdown("숨김")
+import streamlit as st
+
+# Create a title for the collapsible section
+title = "오늘자 화재 현황"
+
+# Create the content that you want to hide/reveal
+hidden_content = st.markdown("""<body><div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; "><iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -600px; width: 1200px;"></iframe></div></body>""", unsafe_allow_html = True)
+
+# Use the collapsible function to create the fold/unfold button
+st.beta_layout.collapsible(title, False, hidden_content)
 with st.spinner("Loading..."):
     st.markdown("""
     <body>
