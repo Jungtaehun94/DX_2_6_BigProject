a68082967fd6681c32fe86b9962918b7b3668a81
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-19 16:54:04 +0900

    Add files via upload

--- pages/1_🔥실시간_전국_화재발생_현황.py
+++ pages/1_🔥실시간_전국_화재발생_현황.py
@@ -4,29 +4,77 @@ from io import BytesIO
 from urllib import request
 import time
 from add_logo import add_logo
+import numpy as np
+import pandas as pd
 
 url = "https://nfds.go.kr/favicon.ico"
 res = request.urlopen(url).read()
-
 im = Image.open(BytesIO(res))
 st.set_page_config(page_icon=im, layout="wide")
+
+
 add_logo()
 
-# Create a title for the collapsible section
-title = "오늘자 화재 현황"
 
-# Create the content that you want to hide/reveal
-# hidden_content = 
 
-# Use the collapsible function to create the fold/unfold button
-expander = st.expander(title)
+ques = st.sidebar.radio(
 
-# Use an if statement to check the state of the expander
-with expander:
-    st.markdown("""<body><div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; "><iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -600px; width: 1200px;"></iframe></div></body>""", unsafe_allow_html = True)
+    "선택하세요",
+
+    ('한 눈에 보는 화재현황','전국출동현황','전국 화재 현황'))
+
+
+if ques == '한 눈에 보는 화재현황':
     
-with st.spinner("Loading..."):
+    st.markdown("### 한 눈에 보는 화재 현황")
     st.markdown("""
+<body>
+    <div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px; ">
+        <iframe scrolling="no" src=https://nfds.go.kr/dashboard/quicklook.do style="border: 0px none; margin-left: 0px; height: 1380px; margin-top: -600px; width: 1200px;">
+        </iframe>
+    </div>
+</body>
+""", unsafe_allow_html = True)
+
+elif ques =='전국출동현황':
+    st.markdown("### 서울 출동 건수")
+    st.sidebar.header("서울 출동 건수")
+    st.write(
+    """### 구조+화재+구급+재난""")
+    st.markdown("### 일별")
+
+ 
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+    st.markdown("### 사고별")
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 4),
+    columns=['화재', '구조', '시위','구급'])
+
+    st.area_chart(chart_data)
+
+
+else :
+
+
+
+
+    with st.spinner("Loading..."):
+        
+        st.markdown("""
     <body>
         <div style="border: 0px none; overflow: hidden; margin: 15px auto; max-width: 1600px; ">
             <iframe scrolling="no" src=https://nfds.go.kr/dashboard/monitor.do style="border: 0px none; margin-left: 0px; height: 2160px; width: 100%; margin-top: -550px; width: 1200px;">
@@ -34,7 +82,40 @@ with st.spinner("Loading..."):
         </div>
     </body>
     """, unsafe_allow_html = True)
-    time.sleep(3)
+        time.sleep(3)
     
+
+
+# else:
+#     st.markdown("### 서울 출동 건수")
+# st.sidebar.header("서울 출동 건수")
+# st.write(
+#     """### 구조+화재+구급+재난""")
+# st.markdown("### 일별")
+
+ 
+# progress_bar = st.sidebar.progress(0)
+# status_text = st.sidebar.empty()
+# last_rows = np.random.randn(1, 1)
+# chart = st.line_chart(last_rows)
+
+# for i in range(1, 101):
+    
+#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+#     status_text.text("%i%% Complete" % i)
+#     chart.add_rows(new_rows)
+#     progress_bar.progress(i)
+#     last_rows = new_rows
+#     time.sleep(0.05)
+
+#     progress_bar.empty()
+#     st.markdown("### 사고별")
+#     chart_data = pd.DataFrame(
+#     np.random.randn(20, 4),
+#     columns=['화재', '구조', '시위','구급'])
+
+#     st.area_chart(chart_data)
+
+
 # http://nfds.go.kr/dashboard/monitor.do
 # <div style="frameBorder=0; overflow: hidden; margin: 15px auto; max-width: 1600px;"></div>
