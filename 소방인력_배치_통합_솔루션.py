34e9f56542b88a434656516e15326fa1b6638837
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-21 00:06:51 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -296,8 +296,8 @@ else:
 
     with cols[0]:
         mapping_demo()
-        st.markdown("""<div style="margin-left:30px;margin-top:-650px;z-index:999;position:relative;width: 300px;height:50px;background-color: rgba(255,255,255,.2);display: flex;align-items: center;">
-  <ul style="display:flex;flex-direction:row;width: 250px;align-items: center;">
+        st.markdown("""<div style="margin-left:30px;margin-top:-650px;z-index:999;position:relative;width: 300px;height:50px;background-color: rgba(255,255,255,.5);display: flex;align-items: center;">
+  <ul style="display:flex;flex-direction:row;width: 220px;align-items: center;padding-left:16px;">
     <li style="align-items:center;cursor:pointer;display:flex;flex-direction:row;margin-left:10px;margin-top: auto;align-content: center;"><span style="background:rgba(192,64,64,.8);border-color:#ff6384;border-width:3px;display:inline-block;height:20px;margin-right:10px;width:20px"></span>
       <p style="color:#2f2f2f;margin:0;padding:0">감원 필요</p>
     </li>
