4a6d9a47406558359e945b772d45a7435821cc23
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-20 23:04:09 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -142,7 +142,7 @@ def mapping_demo():
         ddd = pdk.Layer(
                 "TextLayer",
                 data=df_dec,
-                get_position=["lng+0.013", "lat+0.01"],
+                get_position=["lng+0.017", "lat"],
                 get_text="소방공무원_22 + 감원",
                 get_size=30,
                 get_color=[192, 64, 64],
