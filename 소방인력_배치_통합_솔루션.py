1b9445b9fe1dbc7a49aca777285580b9a59ce90b
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-20 22:40:01 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -49,8 +49,7 @@ df_text['오차'] = df_text['오차'].astype(str)
 
 df_text['증원'] = df_text['증원'].astype(str)
 df_text['증원'] = df_text['증원']
-df_text['감원'] = df_text['감원'].abs()
-df_text['감원'] = df_text['감원'].astype(str)
+df['감원'] = df['감원'].apply(lambda x: str(abs(x)))
 df_text['증원'] = '(' + df_text['증원'] + ')'
 df_text['감원'] = '(' + df_text['감원'] + ')'
 df_dpt = pd.read_csv(r"./data2.csv", encoding = 'cp949')
