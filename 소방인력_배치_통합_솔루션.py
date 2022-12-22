212587e13eeb03353eff6efadebf1e57860c62e5
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-22 13:21:29 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -518,7 +518,7 @@ if to_show == "실시간 출동 현황":
     with ne_cols[1]:
         mapping_demo()
         st.markdown(
-            """<div style="margin-left: 28px;margin-top: -385px;z-index: 999;position: relative;width: 376px;height: 50px;background-color: rgba(0, 0, 0, 0.5);">
+            """<div style="margin-left: 12px;margin-top: -435px;z-index: 999;position: relative;width: 376px;height: 50px;background-color: rgba(0, 0, 0, 0.5);">
             <ul style="display: flex;flex-direction: row;width: 342px;align-items: center;padding-left: 0px;padding-top: 7px;padding-bottom: 7px;">
             <li style="align-items: center; cursor: pointer; display: flex; flex-direction: row; margin-left: 10px; margin-top: auto; align-content: center;"><span style="background: rgba(192, 64, 64, 0.8); border-color: rgb(255, 99, 132); border-width: 3px; display: inline-block; height: 20px; margin-right: 10px; width: 20px;"></span>
             <p style="color: rgb(218, 218, 218); margin: 0px; padding: 0px; font-family: &quot;Noto Sans KR&quot;, sans-serif; font-size: 18px;">지원 출동 소방서</p>
