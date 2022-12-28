a013c0e670a07111d416eb7f7de5261bf35f7bc9
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-28 12:31:46 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -48,7 +48,7 @@ df = pd.read_csv(r"./data.csv", encoding="cp949")
 df_text = df.copy()
 df_text["증원"] = df_text["증원"].where(df_text["증원"] > 0, "")
 df_text["감원"] = df_text["감원"].where(df_text["감원"] < 0, "")
-df_text
+df_text.info()
 df_text["현원"] = df_text["현원"].astype(str)
 
 df_text["오차"] = df_text["오차"].astype(str)
