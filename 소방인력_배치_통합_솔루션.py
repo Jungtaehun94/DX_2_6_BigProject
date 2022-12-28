4729dfd1ba59979f15736db40da7132ecda10cd4
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2022-12-28 13:09:13 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -599,14 +599,14 @@ if to_show == "재난 발생시":
 #     latest_time_hr = (latest_time_hr - 12) if latest_time_hr > 12 else latest_time_hr
 #     with new_ne_cols[1]:
     @st.experimental_memo(experimental_allow_widgets=True)
-    def cached_chart_by_slider(slider):
+    def cached_chart_by_slider():
         slider = new_ne_cols[1].slider("조회할 시간대 선택:", 0, 23, step=1)
         bar = alt.Chart(df_4_chart3[df_4_chart3["시간대구분"] == slider]).mark_bar().encode(
             x=alt.X('행정동코드:N', axis=alt.Axis(title='자치구')),
             y=alt.Y('유동인구:Q', axis=alt.Axis(title='유동인구'), scale=alt.Scale(domain=[0,1100000]))
         ).properties(height=600)
         new_ne_cols[0].altair_chart(bar, use_container_width=True, theme="streamlit")
-    cached_chart_by_slider
+    cached_chart_by_slider()
 #             
 #             st.bar_chart(df_4_chart3[df_4_chart3["시간대구분"] == slider], x="행정동코드", y="유동인구")
                 
