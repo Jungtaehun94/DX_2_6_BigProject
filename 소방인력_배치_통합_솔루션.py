43ec79e2a5abd6d2783a4b9a4d3639085a05118d
Jungtaehun94 <112460411+Jungtaehun94@users.noreply.github.com>
2023-01-03 20:50:57 +0900

    Update 소방인력_배치_통합_솔루션.py

--- 소방인력_배치_통합_솔루션.py
+++ 소방인력_배치_통합_솔루션.py
@@ -103,7 +103,7 @@ df_dpt, supp_list = fix_rand()
 gu_loc = df_dpt.columns.get_loc('출동소방서')
 
 with st.sidebar:
-    to_show = st.radio("유형별", ("평시", "재난 발생시"))
+    to_show = st.radio("상황별", ("평시", "재난 발생시"))
 # find_close_points(df_dpt,df_dpt.sample(1).reset_index()['출동소방서'][0],3)
 
 def mapping_demo():
